8d4354b8194d36b57e705f6670e358874634fee0
Nikolozi Matsaberidze <nikolozi_matsaberidze@epam.com>
2026-02-19 13:19:30 +0400

    Removed unnecessary locks from SubNode class

--- src/main/java/com/bachelor/preprocess/SubNode.java
+++ src/main/java/com/bachelor/preprocess/SubNode.java
@@ -6,9 +6,7 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class SubNode implements Iterable<SubNode>{
-    private final ReadWriteLock LOCK = new ReentrantReadWriteLock();
-    private final Lock readLock = LOCK.readLock();
-    private final Lock writeLock = LOCK.writeLock();
+
 
     // -> T[i] where i is the index of the node in the Euler chain.
     private int nodeInfo; // reference to the corresponding TreeNode inside the T array.
@@ -42,102 +40,60 @@ public class SubNode implements Iterable<SubNode>{
     }
 
     public int getNodeInfo() {
-        readLock.lock();
-        try {
             return nodeInfo;
-        } finally {
-            readLock.unlock();
-        }
+
     }
 
     public void setNodeInfo(int nodeInfo) {
-        writeLock.lock();
-        try {
             this.nodeInfo = nodeInfo;
-        } finally {
-            writeLock.unlock();
-        }
+
     }
 
     public SubNode getTourInfo() {
-        readLock.lock();
-        try {
             return tourInfo;
-        } finally {
-            readLock.unlock();
-        }
+
     }
 
     public void setTourInfo(SubNode tourInfo) {
-        writeLock.lock();
-        try {
             this.tourInfo = tourInfo;
-        } finally {
-            writeLock.unlock();
-        }
+
     }
 
     public int getSubtree() {
-        readLock.lock();
-        try {
+
             return subtree;
-        } finally {
-            readLock.unlock();
-        }
+
     }
 
     public void setSubtree(int subtree) {
-        writeLock.lock();
-        try {
+
             this.subtree = subtree;
-        } finally {
-            writeLock.unlock();
-        }
+
     }
 
     public NodeType getType() {
-        readLock.lock();
-        try {
+
             return type;
-        } finally {
-            readLock.unlock();
-        }
+
     }
 
     public void setType(NodeType type) {
-        writeLock.lock();
-        try {
+
             this.type = type;
-        } finally {
-            writeLock.unlock();
-        }
     }
 
     public int getCost() {
-        readLock.lock();
-        try{
+
             return cost;
-        } finally {
-            readLock.unlock();
-        }
+
     }
 
     public void setCost(int cost) {
-        writeLock.lock();
-        try{
+
             this.cost = cost;
-        } finally {
-            writeLock.unlock();
-        }
+
     }
 
-    public int getCostWithoutLock() {
-        return cost;
-    }
-
-    public void setCostWithoutLock(int cost) {
-        this.cost = cost;
-    }
 
     @Override
     public String toString() {
@@ -147,16 +103,13 @@ public class SubNode implements Iterable<SubNode>{
         int subtreeCopy;
         NodeType typeCopy;
         int costCopy;
-        readLock.lock();
-        try {
+
             nodeInfoCopy = this.nodeInfo;
             tourInfoCopy = this.tourInfo;
             subtreeCopy = this.subtree;
             typeCopy = this.type;
             costCopy = this.cost;
-        } finally {
-            readLock.unlock();
-        }
+
 
         String tourInfoStr = (tourInfoCopy == null) ? "null" : "SubNode@" + Integer.toHexString(System.identityHashCode(tourInfoCopy));
 
@@ -170,34 +123,20 @@ public class SubNode implements Iterable<SubNode>{
     }
 
     public SubNode getNext() {
-        readLock.lock();
-        try{
+
             return next;
-        } finally {
-            readLock.unlock();
-        }
+
     }
 
     public void setNext(SubNode next) {
-        writeLock.lock();
-        try{
+
             this.next = next;
-        } finally {
-            writeLock.unlock();
-        }
+
     }
 
-    public SubNode getNextWithoutLock() {
-        return next;
-    }
 
-    public void setNextWithoutLock(SubNode next) {
-        this.next = next;
-    }
 
-    public Lock getWriteLock(){
-        return this.writeLock;
-    }
+
 
     private class Itr implements Iterator<SubNode>{
         private SubNode current;
